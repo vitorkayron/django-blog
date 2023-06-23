5a6b2e2d241bc683a5242e45d060046486774246
Vitor Kayron <vitork2016@gmail.com>
2023-06-23 13:46:54 -0300

    Update Views and Url

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Post(models.Model):
     title = models.CharField(max_length=100, unique=True, blank=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
